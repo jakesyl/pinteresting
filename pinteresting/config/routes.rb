2a1f70ae8b8ff3637e33dae6945750cc53f5c60d
jakesyl <jakesyl@gmail.com>
2014-06-26 17:56:39 -0400

    added contact page

--- pinteresting/config/routes.rb
+++ pinteresting/config/routes.rb
@@ -3,6 +3,7 @@ root "pages#home"
 
 #getting somenew pages
 get "about" => "pages#about"
+get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
