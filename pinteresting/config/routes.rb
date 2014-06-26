a4fc673df407b9bce1df014afd0c6f5ce93e2950
jakesyl <jakesyl@gmail.com>
2014-06-25 23:56:00 -0400

    up to day 6

--- pinteresting/config/routes.rb
+++ pinteresting/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 root "pages#home"
+
+#getting somenew pages
+get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
