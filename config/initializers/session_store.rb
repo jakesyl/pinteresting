12ba693c26ab1174ccee2fd2bc88bd4f49fa341d
jakesyl <jakesyl@gmail.com>
2014-07-14 21:58:56 -0400

    debugging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pinteresting_session'
+Omrails::Application.config.session_store :cookie_store, key: '_omrails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Omrails::Application.config.session_store :active_record_store
