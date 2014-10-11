4ba4c62175e27e67cb942d451c765ae86a9c6288
Jake Sylvestre <jakesyl@gmail.com>
2014-10-11 01:51:34 -0400

    clever

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root "pages#home"#Does whitespace matter in ruby, ah who the hell cares
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
